656679da6e20eaccb104959fa8a59709a4649965
Kebie <nigel.kehler@gmail.com>
2014-04-11 10:23:23 -0700

    finished the D level, setup bootstrap, devise, and a bunch of other stuff, made the user, ideas, and comments models, and connected them

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,8 @@
+class IdeasController < ApplicationController
+  def index
+    
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Idealand::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+
+  #route
+  root "ideas#index"
+
+  resources :ideas do
+    resources :comments
+  end
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
