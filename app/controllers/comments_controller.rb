9700975cf08f02b98f4160d7a55a581481184698
Kebie <nigel.kehler@gmail.com>
2014-04-11 11:41:27 -0700

    adding of comments, and ideas works, LEVEL C Complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,41 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy, :edit]
+
+  before_action :find_comment, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @idea = Idea.find params[:idea_id]
+    @comment = @idea.comments.new(comment_attributes)
+    @comment.user = current_user #connect a user to the comment
+    
+    if @comment.save
+      redirect_to @idea, notice: "Your thrilling comment was added"
+    else
+      @comments = @idea.comments
+      render "/ideas/show"
+    end
+
+  end
+
+  def destroy
+    if @comment.destroy
+      redirect_to @idea, notice: "Comment destroyed"
+    else
+      redirect_to @idea, notice: "There was a problem deleting your comment"
+    end
+    
+  end
+
+
+  private
+  def find_comment
+    @comment = Comment.find(params[:comment_id] || params[:id])
+    @idea = @comment.idea
+  end
+
+  def comment_attributes
+    params.require(:comment).permit(:comment)
+  end
+
+
 end
--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,57 @@
 class IdeasController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :find_idea, only: [:edit, :destroy, :update]
+
   def index
-    
+    @ideas = Idea.all
+  end
+
+  def show
+    @idea = Idea.find(params[:id]) #have to get the idea not from a user can any can view
+    @comments = @idea.comments
+    @comment = Comment.new
+  end
+
+  def create
+    @idea = current_user.ideas.new(idea_attributes)
+    if @idea.save
+      redirect_to ideas_path, notice: "Your terrible idea was added."
+    else
+      render :new
+    end
   end
 
 
+  def update
+    if @idea.update_attributes(idea_attributes)
+      redirect_to @idea, notice: "Your idea was updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @idea.destroy
+      redirect_to ideas_path, notice: "Idea deleted."
+    else
+      redirect_to ideas_path, error: "We had a problem deleting your idea"
+    end    
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  private
+
+  def idea_attributes
+    params.require(:idea).permit(:title, :description)
+  end
+
+  def find_idea
+    @idea = current_user.ideas.find(params[:id]) #maybe later if we have to lock down ideas change to
+                                  # current_user.ideas.find(params[:id])
+  end
 
 end
